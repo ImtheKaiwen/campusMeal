9d0d4b1ed498ebfef677ba31a10002640e635e3d
Kerem <bucocuknereli@gmail.com>
2025-11-14 15:34:46 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -5,15 +5,14 @@ bp = Blueprint("bp", __name__)
 
 @bp.route("/getlatestmenu", methods=["GET"])
 def get_latest_menu():
-    try:
-        menu = set_new_list()
-        today_menu = get_today_menu(menu)
-        if today_menu:
-            return jsonify(menu)
-        else:
-            return Response(
-                jsonify({"message": "Bugün için menü bulunamadı."}).get_data(as_text=True),
-                mimetype='application/json'
-            )
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    menu = set_new_list()
+    today_menu = get_today_menu(menu)
+
+    if today_menu:
+        return jsonify(menu)
+    else:
+        return Response(
+            json.dumps({"message": "Bugün için menü bulunamadı."}, ensure_ascii=False),
+            mimetype='application/json'
+        )
+
